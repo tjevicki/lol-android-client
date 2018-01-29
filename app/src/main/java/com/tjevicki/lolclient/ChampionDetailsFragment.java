be9917a4ddac93d85df92797c261abea1f96b3ef
Tibor Jevicki <tjevicki@outlook.com>
2018-01-29 22:17:42 +0100

    Create Champion details fragment

---
+++ app/src/main/java/com/tjevicki/lolclient/ChampionDetailsFragment.java
@@ -0,0 +1,23 @@
+package com.tjevicki.lolclient;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tjevi on 29-Jan-18.
+ */
+
+public class ChampionDetailsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.champion_details_fragment, container, false);
+
+        return view;
+    }
+}
